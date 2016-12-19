5471486a2ed5d761a4638a86c6a10527320ce043
FerreWagner <18408229270@163.com>
2016-12-19 11:12:00 +0800

    Create config.php

---
+++ PDO/config.php
@@ -0,0 +1,11 @@
+<?php
+    define("DB_HOST", 'localhost');
+    define("DB_USER", 'root');
+    define("DB_PWD", 'fate');
+    define("DB_NAME", 'nemo');
+    define("DB_PORT", '80');
+    define("DB_TYPE", 'mysql');
+    define("DB_CHARSET", 'utf8');
+    
+
+?>
